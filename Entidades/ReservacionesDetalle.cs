d4870b37e43bd03d3085952115383843bd731bfa
Felix <felix_reynoso1@ucne.edu.do>
2021-11-14 13:42:01 -0400

    Modificando datos de algunas entidades

--- Entidades/ReservacionesDetalle.cs
+++ Entidades/ReservacionesDetalle.cs
@@ -14,11 +14,36 @@ namespace HotelMiraflores.Entidades
         public int ProductoID { get; set; }
         public int Cantidad { get; set; }
         public float Precio { get; set; }
+        public float TotalProducto { get; set; }
 
         [ForeignKey("ReservacionID")]
         public virtual Reservaciones Reservacion { get; set; }
 
         [ForeignKey("ProductoID")]
         public virtual Productos Producto  { get; set; }
+
+        public ReservacionesDetalle()
+        {
+            ID = 0;
+            ReservacionID = 0;
+            ProductoID = 0;
+            Cantidad = 0;
+            Precio = 0;
+            TotalProducto = 0;
+            Reservacion = null;
+            Producto = null;
+        }
+
+        public ReservacionesDetalle(int ReservacionId, int ProductoId, int cantidad, float precio, float totalproducto, Reservaciones reservacion, Productos producto)
+        {
+            ID = 0;
+            ReservacionID = ReservacionId;
+            ProductoID = ProductoId;
+            Cantidad = cantidad;
+            Precio = precio;
+            TotalProducto = totalproducto;
+            Reservacion = reservacion;
+            Producto = producto;
+        }
     }
 }

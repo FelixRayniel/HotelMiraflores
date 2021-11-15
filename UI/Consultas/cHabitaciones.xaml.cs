8bb780c8f87659040827b28edfa6ef58df6e7398
Felix <felix_reynoso1@ucne.edu.do>
2021-11-15 18:19:52 -0400

    Agregando parte de las cosultas del proyecto.

---
+++ UI/Consultas/cHabitaciones.xaml.cs
@@ -0,0 +1,56 @@
+﻿using HotelMiraflores.BLL;
+using HotelMiraflores.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelMiraflores.UI.Consultas
+{
+    /// <summary>
+    /// Lógica de interacción para cHabitaciones.xaml
+    /// </summary>
+    public partial class cHabitaciones : Window
+    {
+        public cHabitaciones()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<Habitaciones>();
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0:
+                        listado = HabitacionesBLL.GetList(s => s.HabitacionID == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+
+                    case 1:
+                        listado = HabitacionesBLL.GetList(s => s.Numero.ToLower().Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                }
+            }
+            else
+            {
+                listado = HabitacionesBLL.GetList(c => true);
+            }
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
+
+        }
+
+    }
+}
---
+++ UI/Consultas/cSuplidores.xaml.cs
@@ -0,0 +1,55 @@
+﻿using HotelMiraflores.BLL;
+using HotelMiraflores.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelMiraflores.UI.Consultas
+{
+    /// <summary>
+    /// Lógica de interacción para cSuplidores.xaml
+    /// </summary>
+    public partial class cSuplidores : Window
+    {
+        public cSuplidores()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<Suplidores>();
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0:
+                        listado = SuplidoresBLL.GetList(s => s.SuplidorID == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+
+                    case 1:
+                        listado = SuplidoresBLL.GetList(s => s.Nombre.ToLower().Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                }
+            }
+            else
+            {
+                listado = SuplidoresBLL.GetList(c => true);
+            }
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
+
+        }
+    }
+}

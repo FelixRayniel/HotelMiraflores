6d8980e754398e7c896759682964c8f4af6c7772
Perla D. Canario Perez <perladenis163@gmail.com>
2021-11-15 17:33:46 -0400

    Agregando consulta de Huespedes

---
+++ UI/Consultas/cHuespedes.xaml.cs
@@ -0,0 +1,58 @@
+﻿using HotelMiraflores.BLL;
+using HotelMiraflores.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelMiraflores.UI.Consultas
+{
+    /// <summary>
+    /// Lógica de interacción para cHuespedes.xaml
+    /// </summary>
+    public partial class cHuespedes : Window
+    {
+        public cHuespedes()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<Huespedes>();
+            string criterio = CriterioTextBox.Text.Trim();
+
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0:
+                        listado = HuespedesBLL.GetList(p => true);
+                        break;
+                    case 1:
+                        listado = HuespedesBLL.GetList(p => p.Cedula.ToLower().Contains(criterio.ToLower()));
+                        break;
+                    case 2:
+                        listado = HuespedesBLL.GetList(p => p.HuespedID == Convert.ToInt32(CriterioTextBox.Text));
+                        break;
+                }
+            }
+            else
+            {
+                listado = HuespedesBLL.GetList(p => true);
+            }
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
+        }
+    }
+}

a87285ceadfabb07dd4fd3ac8282c8485784f763
Perla D. Canario Perez <perladenis163@gmail.com>
2021-11-18 16:38:04 -0400

    Agregando Consutas de Productos y TipoHacitaciones

---
+++ UI/Consultas/cProductos.xaml.cs
@@ -0,0 +1,61 @@
+﻿using HotelMiraflores.BLL;
+using HotelMiraflores.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelMiraflores.UI.Consultas
+{
+    /// <summary>
+    /// Lógica de interacción para cProductos.xaml
+    /// </summary>
+    public partial class cProductos : Window
+    {
+        public cProductos()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<Productos>();
+            string criterio = CriterioTextBox.Text.Trim();
+
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0:
+                        listado = ProductosBLL.GetList(p => true);
+                        break;
+                    case 1:
+                        listado = ProductosBLL.GetList(p => p.DepartamentoID == Convert.ToInt32(CriterioTextBox.Text));
+                        break;
+                    case 2:
+                        listado = ProductosBLL.GetList(p => p.ProductoId == Convert.ToInt32(CriterioTextBox.Text));
+                        break;
+                    case 3:
+                        listado = ProductosBLL.GetList(p => p.SuplidorID == Convert.ToInt32(CriterioTextBox.Text));
+                        break;
+                }
+            }
+            else
+            {
+                listado = ProductosBLL.GetList(p => true);
+            }
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
+        }
+    }
+}
---
+++ UI/Consultas/cTipoHabitaciones.xaml.cs
@@ -0,0 +1,58 @@
+﻿using HotelMiraflores.BLL;
+using HotelMiraflores.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelMiraflores.UI.Consultas
+{
+    /// <summary>
+    /// Lógica de interacción para cTipoHabitaciones.xaml
+    /// </summary>
+    public partial class cTipoHabitaciones : Window
+    {
+        public cTipoHabitaciones()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<TipoHabitaciones>();
+            string criterio = CriterioTextBox.Text.Trim();
+
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0:
+                        listado = TipoHabitacionesBLL.GetList(p => true);
+                        break;
+                    case 1:
+                        listado = TipoHabitacionesBLL.GetList(p => p.Descripcion.ToLower().Contains(criterio.ToLower()));
+                        break;
+                    case 2:
+                        listado = TipoHabitacionesBLL.GetList(p => p.TipoHabitacionId == Convert.ToInt32(CriterioTextBox.Text));
+                        break;
+                }
+            }
+            else
+            {
+                listado = TipoHabitacionesBLL.GetList(p => true);
+            }
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
+        }
+    }
+}

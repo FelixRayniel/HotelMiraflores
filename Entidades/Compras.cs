8471f8f8103739958f8f229b3ee401e60d14c6b5
Perla D. Canario Perez <perladenis163@gmail.com>
2021-11-20 21:13:15 -0400

    Agregando Entidades Compras y comprasDetalle

---
+++ Entidades/Compras.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelMiraflores.Entidades
+{
+    public class Compras
+    {
+        [Key]
+
+        public int CompraId { get; set; }
+        public int SuplidorId { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public float TotalCompra { get; set; }
+        public int UsuarioId { get; set; }
+
+        [ForeignKey("ProductoId")]
+        public virtual Productos Producto { get; set; }
+
+        [ForeignKey("ComprasId")]
+        public List<ComprasDetalle> ComprasDetalle { get; set; } = new List<ComprasDetalle>();
+
+    }
+}
---
+++ Entidades/ComprasDetalle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelMiraflores.Entidades
+{
+    public class ComprasDetalle
+    {
+        public int Id { get; set; }
+        public int ProductoId { get; set; }
+        public int CompraId { get; set; }
+        public float Costo { get; set; }
+        public int Cantidad { get; set; }
+        public float CantidadDisponible { get; set; }
+        public int UsuarioId { get; set; }
+        public float SubtotalCompra
+        {
+            get
+            {
+                return Cantidad * Costo;
+            }
+        }
+        [ForeignKey("CompraId")]
+        public virtual Compras Compras { get; set; }
+
+        public ComprasDetalle()
+        {
+            Id = 0;
+            ProductoId = 0;
+            CompraId = 0;
+            Costo = 0;
+            Cantidad = 0;
+            CantidadDisponible = 0;
+        }
+
+        public ComprasDetalle(int productoId, int compraId, float costo, int cantidad, float cantidadDisponible)
+        {
+            Id = 0;
+            ProductoId = productoId;
+            CompraId = compraId;
+            Costo = costo;
+            Cantidad = cantidad;
+            CantidadDisponible = cantidadDisponible;
+        }
+    }
+}

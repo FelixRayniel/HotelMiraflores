0e3b7bef5556159e46e06ff4eb542d53c796220d
Felix <felix_reynoso1@ucne.edu.do>
2021-11-29 18:18:22 -0800

    Reconfigurando Algunas Consultas.

---
+++ UI/Consultas/cReservaciones.xaml.cs
@@ -0,0 +1,61 @@
+﻿using HotelMiraflores.BLL;
+using HotelMiraflores.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelMiraflores.UI.Consultas
+{
+    /// <summary>
+    /// Interaction logic for cReservaciones.xaml
+    /// </summary>
+    public partial class cReservaciones : Window
+    {
+        public cReservaciones()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<Reservaciones>();
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0:
+                        listado = ReservacionesBLL.GetList(r => r.ReservacionId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+
+                    case 1:
+                        listado = ReservacionesBLL.GetList(r => r.HuespedId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+                }
+            }
+            else
+            {
+                listado = ReservacionesBLL.GetList(c => true);
+            }
+
+            if (DesdeDataPicker.SelectedDate != null)
+                listado = ReservacionesBLL.GetList(c => c.Fecha.Date >= DesdeDataPicker.SelectedDate);
+
+            if (HastaDatePicker.SelectedDate != null)
+                listado = ReservacionesBLL.GetList(c => c.Fecha.Date <= HastaDatePicker.SelectedDate);
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
+        }
+    }
+    
+}
---
+++ UI/Consultas/cUsuarios.xaml.cs
@@ -0,0 +1,57 @@
+﻿using HotelMiraflores.BLL;
+using HotelMiraflores.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelMiraflores.UI.Consultas
+{
+    /// <summary>
+    /// Interaction logic for cUsuarios.xaml
+    /// </summary>
+    public partial class cUsuarios : Window
+    {
+        public cUsuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<Usuarios>();
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0:
+                        listado = UsuariosBLL.GetList(s => s.UsuarioId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+
+                    case 1:
+                        listado = UsuariosBLL.GetList(s => s.Nombres.ToLower().Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                }
+            }
+            else
+            {
+                listado = UsuariosBLL.GetList(c => true);
+            }
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
+
+        }
+
+    }
+}
+

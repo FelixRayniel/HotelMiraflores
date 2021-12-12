48d6e20e8799391214afbe6cc7a610fc00ce84c6
Felix <felix_reynoso1@ucne.edu.do>
2021-12-12 11:00:02 -0400

    Ultimos Cambios en las consultas.

--- UI/Consultas/cReservaciones.xaml.cs
+++ UI/Consultas/cReservaciones.xaml.cs
@@ -59,6 +59,14 @@ namespace HotelMiraflores.UI.Consultas
 
             DatosDataGrid.ItemsSource = null;
             DatosDataGrid.ItemsSource = listado;
+
+            var conteo = listado.Count();
+            ConteoTextBox.Text = conteo.ToString();
+
+
+            var monto = listado.Sum(x => x.TotalGeneral);
+            TotalTextBox.Text = monto.ToString("N2");
+
         }
     }
     
